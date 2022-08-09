84c22f722213a409773dde1a9a8502f58d71c7bf
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-09 23:30:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import { Routes, Route, Link } from "react-router-dom";
+
 // eslint-disable-next-line react/prefer-stateless-function
 function App() {
   return (
+    <>
+<Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+</Routes>
 
     <Calculator />
+    </>
   );
 }
 
